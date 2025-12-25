ad2f97a9140aa1635380456f6f076f394250f1d9
DESKTOP-991F2DU\ADMIN <taiho.310805@gmail.com>
2025-12-25 21:22:51 +0700

    cap nhat

--- QuanLyChiTieu (2)/QuanLyChiTieu/QuanLyChiTieu/QuanLyChiTieu/app/src/main/java/com/example/webmasterdotnetvn/quanlychitieu/UserDatabase.java
+++ QuanLyChiTieu (2)/QuanLyChiTieu/QuanLyChiTieu/QuanLyChiTieu/app/src/main/java/com/example/webmasterdotnetvn/quanlychitieu/UserDatabase.java
@@ -1,7 +1,7 @@
 // File: UserDatabase.java
 package com.example.webmasterdotnetvn.quanlychitieu; // Giữ đúng package của bạn
 
-// Lớp này dùng để "giả lập" một db
+// Lớp này dùng để "giả lập" một data base,
 // Nó dùng biến static để giữ thông tin,
 // thông tin này sẽ mất khi app bị tắt hẳn.
 public class UserDatabase {
