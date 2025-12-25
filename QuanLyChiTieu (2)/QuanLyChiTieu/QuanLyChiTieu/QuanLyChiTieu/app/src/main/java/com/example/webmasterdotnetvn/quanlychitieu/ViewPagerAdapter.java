99bd1d8eea978ad92fb49910697efb7c43cd5577
DESKTOP-991F2DU\ADMIN <taiho.310805@gmail.com>
2025-12-25 21:01:35 +0700

    cap nhat

--- QuanLyChiTieu (2)/QuanLyChiTieu/QuanLyChiTieu/QuanLyChiTieu/app/src/main/java/com/example/webmasterdotnetvn/quanlychitieu/ViewPagerAdapter.java
+++ QuanLyChiTieu (2)/QuanLyChiTieu/QuanLyChiTieu/QuanLyChiTieu/app/src/main/java/com/example/webmasterdotnetvn/quanlychitieu/ViewPagerAdapter.java
@@ -16,9 +16,9 @@ public class ViewPagerAdapter extends FragmentStateAdapter {
     public Fragment createFragment(int position) {
         // position 0 là tab đầu tiên, 1 là tab thứ hai
         if (position == 0) {
-            return new NhomChiFragment(); // Trả về Fragment Nhóm Chi
+            return new NhomChiFragment(); // Trả về Fragment của Nhóm Chi
         } else {
-            return new MucChiFragment(); // Trả về Fragment Mục Chi
+            return new MucChiFragment(); // Trả về Fragment của Mục Chi
         }
     }
 
