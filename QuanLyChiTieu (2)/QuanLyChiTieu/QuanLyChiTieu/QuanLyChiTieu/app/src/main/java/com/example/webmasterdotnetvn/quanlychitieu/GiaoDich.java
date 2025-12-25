fa89ac5928ac699088520b6e2f73efde11a29503
DESKTOP-NC78VN5\ADMIN <phanhuynhvietthien722005@gmail.com>
2025-12-25 21:06:26 +0700

    cap nhat code

--- QuanLyChiTieu (2)/QuanLyChiTieu/QuanLyChiTieu/QuanLyChiTieu/app/src/main/java/com/example/webmasterdotnetvn/quanlychitieu/GiaoDich.java
+++ QuanLyChiTieu (2)/QuanLyChiTieu/QuanLyChiTieu/QuanLyChiTieu/app/src/main/java/com/example/webmasterdotnetvn/quanlychitieu/GiaoDich.java
@@ -23,7 +23,7 @@ public class GiaoDich implements Serializable {
         this.type = type;
     }
 
-    
+
     public String getId() { return id; }
     public void setId(String id) { this.id = id; }
 
